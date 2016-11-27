e7adc710938b940b5c3754c71301b7dca5a0a17c
Jack Flannery <jmflannery81@gmail.com>
2016-11-27 11:39:28 -0500

    Move admin post controller functionality to post controller and remove admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   mount Toke::Engine => "/"
 
-  resources :posts, only: [:index, :show]
-
-  namespace :admin do
-    resources :posts
-  end
+  resources :posts
 end
--- test/integration/posts_test.rb
+++ test/integration/posts_test.rb
@@ -8,56 +8,273 @@ class PostsTest < ActiveSupport::TestCase
     Rack::Builder.parse_file('config.ru').first
   end
 
+  let(:current_user) { FactoryGirl.create(:user) }
+  let(:token) { FactoryGirl.create(:token, user: current_user, expires_at: 1.year.from_now) }
+
+  describe "Create" do
+
+    let(:attrs) {{ title: 'My Post', content: 'This post rocks!' }}
+
+    describe 'given a valid Token' do
+
+      before do
+        token.generate_key!
+        token.save
+        header 'Authorization', "Bearer #{token.key}"
+      end
+
+      describe "given valid attributes" do
+
+        it "creates a post" do
+          post 'posts', post: attrs
+          last_response.status.must_equal 201
+          response = JSON.parse(last_response.body)['post']
+          response['id'].to_s.must_match /\d+/
+          response['title'].must_equal 'My Post'
+          response['content'].must_equal 'This post rocks!'
+        end
+      end
+
+      describe "given invalid attributes" do
+
+        let(:invalid_attrs) {{ post: { title: '', content: 'This post rocks!' }}}
+
+        it "returns 400 Bad Request with a hash of errors" do
+          post 'posts', invalid_attrs
+          last_response.status.must_equal 400
+          response = JSON.parse(last_response.body)['errors']
+          response['title'].must_equal ["can't be blank"]
+        end
+      end
+    end
+
+    describe 'given an invalid Token' do
+      before do
+        header 'Authorization', "Bearer wrong"
+      end
+
+      it 'returns 401 Unauthorized' do
+        post 'posts', post: attrs
+        last_response.status.must_equal 401
+      end
+    end
+  end
+
   describe "Index" do
 
-    let(:published1) { FactoryGirl.create(:published_post) }
-    let(:published2) { FactoryGirl.create(:published_post) }
-    let(:unpublished) { FactoryGirl.create(:post) }
-    setup { [published1, published2, unpublished] }
+    let(:unpublished1) { FactoryGirl.create(:post) }
+    let(:unpublished2) { FactoryGirl.create(:post) }
+    let(:published) { FactoryGirl.create(:published_post) }
+    setup { @post_ids = [unpublished1.id, unpublished2.id, published.id] }
 
-    it "returns only published posts" do
-      get 'posts'
-      last_response.status.must_equal 200
-      response = JSON.parse(last_response.body)['posts']
-      assert_equal 2, response.size
-      ids = response.map { |p| p['id'] }
-      assert_includes ids, published1.id
-      assert_includes ids, published2.id
-      refute_includes ids, unpublished.id
+    describe 'given a valid Token' do
+
+      before do
+        token.generate_key!
+        token.save
+        header 'Authorization', "Bearer #{token.key}"
+      end
+
+      it "returns all posts" do
+        get 'posts'
+        last_response.status.must_equal 200
+        posts = JSON.parse(last_response.body)['posts']
+        assert_equal 3, posts.size
+        ids = posts.map { |p| p['id'] }
+        ids.must_include unpublished1.id
+        ids.must_include unpublished2.id
+        ids.must_include published.id
+      end
+    end
+
+    describe 'given an invalid Token' do
+      before do
+        header 'Authorization', "Bearer wrong"
+      end
+
+      it 'returns only published posts' do
+        get 'posts'
+        last_response.status.must_equal 200
+        posts = JSON.parse(last_response.body)['posts']
+        assert_equal 1, posts.size
+        ids = posts.map { |p| p['id'] }
+        ids.must_include published.id
+      end
     end
   end
 
   describe "Show" do
 
-    describe "given a valid published post id" do
-      let(:post) { FactoryGirl.create(:published_post) }
+    let(:post) { FactoryGirl.create(:post) }
 
-      it "returns the post with the given id" do
-        get "posts/#{post.id}"
-        last_response.status.must_equal 200
-        response = JSON.parse(last_response.body)['post']
-        assert_equal post.id, response['id']
-        assert_equal post.title, response['title']
-        assert_equal post.content, response['content']
+    describe 'given a valid Token' do
+
+      before do
+        token.generate_key!
+        token.save
+        header 'Authorization', "Bearer #{token.key}"
+      end
+
+      describe "given a valid published post id" do
+        let(:post) { FactoryGirl.create(:published_post) }
+
+        it "returns the post with the given id" do
+          get "posts/#{post.id}"
+          last_response.status.must_equal 200
+          response = JSON.parse(last_response.body)['post']
+          assert_equal post.id, response['id']
+          assert_equal post.title, response['title']
+          assert_equal post.content, response['content']
+        end
+      end
+
+      describe "given an unpublished post id" do
+        let(:post) { FactoryGirl.create(:post) }
+
+        it "returns the post with the given id" do
+          get "posts/#{post.id}"
+          last_response.status.must_equal 200
+          response = JSON.parse(last_response.body)['post']
+          assert_equal post.id, response['id']
+          assert_equal post.title, response['title']
+          assert_equal post.content, response['content']
+        end
+      end
+
+      describe "given an invalid post id" do
+
+        it "returns 404 Not Found" do
+          get "posts/12345"
+          last_response.status.must_equal 404
+          last_response.body.must_equal ""
+        end
       end
     end
 
-    describe "given an unpublished post id" do
-      let(:post) { FactoryGirl.create(:post) }
+    describe 'given no Token' do
 
-      it "returns 404 Not Found" do
-        get "posts/#{post.id}"
-        last_response.status.must_equal 404
-        last_response.body.must_equal ""
+      describe "given a valid published post id" do
+        let(:post) { FactoryGirl.create(:published_post) }
+
+        it "returns the post with the given id" do
+          get "posts/#{post.id}"
+          last_response.status.must_equal 200
+          response = JSON.parse(last_response.body)['post']
+          assert_equal post.id, response['id']
+          assert_equal post.title, response['title']
+          assert_equal post.content, response['content']
+        end
+      end
+
+      describe "given an unpublished post id" do
+        let(:post) { FactoryGirl.create(:post) }
+
+        it "returns 404 Not Found" do
+          get "posts/#{post.id}"
+          last_response.status.must_equal 404
+          last_response.body.must_equal ""
+        end
+      end
+    end
+  end
+
+  describe "Update" do
+
+    let(:post) { FactoryGirl.create(:post) }
+    let(:attrs) {{ content: 'The new content' }}
+
+    describe 'given a valid Token' do
+
+      before do
+        token.generate_key!
+        token.save
+        header 'Authorization', "Bearer #{token.key}"
+      end
+
+      describe "given a valid post id and valid attributes" do
+
+        it "updates the post" do
+          put "posts/#{post.id}", post: attrs
+          last_response.status.must_equal 200
+          response = JSON.parse(last_response.body)['post']
+          assert_equal post.id, response['id']
+          assert_equal 'The new content', response['content']
+        end
+      end
+
+      describe "given an invalid post id" do
+
+        it "returns 404 Not Found" do
+          put "posts/12345", post: attrs
+          last_response.status.must_equal 404
+          last_response.body.must_equal ""
+        end
+      end
+
+      describe "given invalid attributes" do
+        let(:attrs) {{ title: '' }}
+
+        it "returns 400 Bad Request with a hash of errors" do
+          put "posts/#{post.id}", post: attrs
+          last_response.status.must_equal 400
+          response = JSON.parse(last_response.body)['errors']
+          assert_equal ["can't be blank"], response['title']
+        end
       end
     end
 
-    describe "given an invalid post id" do
+    describe 'given an invalid Token' do
+      before do
+        header 'Authorization', "Bearer wrong"
+      end
 
-      it "returns 404 Not Found" do
-        get "posts/wrong"
-        last_response.status.must_equal 404
-        last_response.body.must_equal ""
+      it 'returns 401 Unauthorized' do
+        put "posts/#{post.id}", post: attrs
+        last_response.status.must_equal 401
+      end
+    end
+  end
+
+  describe "Delete" do
+
+    let(:post) { FactoryGirl.create(:post) }
+
+    describe 'given a valid Token' do
+
+      before do
+        token.generate_key!
+        token.save
+        header 'Authorization', "Bearer #{token.key}"
+      end
+
+      describe "given a valid post id" do
+
+        it "deletes the post and returns 204 No Content" do
+          delete "posts/#{post.id}"
+          last_response.status.must_equal 204
+          last_response.body.must_equal ""
+          Post.exists?(post.id).must_equal false
+        end
+      end
+
+      describe "given an invalid post id" do
+
+        it "returns 404 Not Found" do
+          delete "posts/12345"
+          last_response.status.must_equal 404
+          last_response.body.must_equal ""
+        end
+      end
+    end
+
+    describe 'given an invalid Token' do
+      before do
+        header 'Authorization', "Bearer wrong"
+      end
+
+      it 'returns 401 Unauthorized' do
+        delete "posts/#{post.id}"
+        last_response.status.must_equal 401
       end
     end
   end
