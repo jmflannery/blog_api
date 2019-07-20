4347f652f3d151bcbffb5d43d265bdbcc1927761
Jack Flannery <jmflannery81@gmail.com>
2019-07-20 01:20:15 -0400

    Add cors origin

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module BlogApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3001', 'blog.jackflannery.me'
+        origins 'localhost:3000', 'localhost:3001', 'blog.jackflannery.me'
         resource '*',
           :methods => [:get, :post, :delete, :put, :patch, :options, :head],
           :headers => :any,
